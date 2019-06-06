ec60f0c152c5e262e44b50aee6a3b18354638b0b
Nathaniel <nathanielmat2@gmail.com>
2019-06-06 19:26:30 +0530

    made page responsive

--- src/components/header.js
+++ src/components/header.js
@@ -28,22 +28,26 @@ const Header = ({ siteTitle }) => (
   dangerouslySetInnerHTML={{
     __html: `
             // When the user scrolls down 100px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
+            window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  //minimized
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("Nav").style.padding = "1.0rem 3.1875rem";
-    document.getElementById("NavList").style.fontSize = "15px";
-    document.getElementById("MozSahyadri").style.fontSize = "1.99em";
-    document.getElementById("NavList").style.margin = "1.7rem 1.1875rem";
-  } else {
-    document.getElementById("Nav").style.padding = "9.0rem 15.1875rem";
-    document.getElementById("NavList").style.fontSize = "20px";
-    document.getElementById("MozSahyadri").style.fontSize = "4.00em";
-    document.getElementById("NavList").style.margin = "4rem 1.1875rem";
-    
+    document.getElementById("Nav").style.padding = "0.2vw";
+    document.getElementById("NavList").style.fontSize = "calc(0.10em + 1.0vw)";
+    document.getElementById("MozSahyadri").style.fontSize = "calc(0.25em + 3.0vw)";
+    document.getElementById("NavList").style.margin = "4.8vw 0vw";
+    document.getElementById("MozSahyadri").style.margin = "calc(0.4em + 0.8vw) 0vw";  
   }
-}
+  //maximized 
+  else {
+    document.getElementById("Nav").style.padding = "calc(5.0rem + 10.0vw)";
+    document.getElementById("NavList").style.fontSize = "calc( 0.3em + 1.2vw)";
+    document.getElementById("MozSahyadri").style.fontSize = "calc(0.80em + 5.0vw)";
+    document.getElementById("NavList").style.margin = "7vw 0vw";
+    document.getElementById("MozSahyadri").style.margin = "1.5vw 0vw";  
+  }
+} 
         `,
   }}
 />
@@ -65,17 +69,18 @@ export default Header
 // function scrollFunction() {
 //   //minimized
 //   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-//     document.getElementById("Nav").style.padding = "1.0rem 3.1875rem";
-//     document.getElementById("NavList").style.fontSize = "calc(0.45em + 0.5vw)";
-//     document.getElementById("MozSahyadri").style.fontSize = "calc(1.50em + 0.7vw)";
-//     document.getElementById("NavList").style.margin = "1.7rem 1.1875rem";
+//     document.getElementById("Nav").style.padding = "0.2vw";
+//     document.getElementById("NavList").style.fontSize = "calc(0.10em + 1.0vw)";
+//     document.getElementById("MozSahyadri").style.fontSize = "calc(0.25em + 3.0vw)";
+//     document.getElementById("NavList").style.margin = "4.8vw 0vw";
+//     document.getElementById("MozSahyadri").style.margin = "calc(0.4em + 0.8vw) 0vw";  
 //   }
 //   //maximized 
 //   else {
-//     document.getElementById("Nav").style.padding = "9.0rem 15.1875rem";
-//     document.getElementById("NavList").style.fontSize = "calc( 0.5em + 1.0vw)";
-//     document.getElementById("MozSahyadri").style.fontSize = "calc(3.00em + 2.0vw)";
-//     document.getElementById("NavList").style.margin = "4rem 1.1875rem";
-    
+//     document.getElementById("Nav").style.padding = "calc(5.0rem + 10.0vw)";
+//     document.getElementById("NavList").style.fontSize = "calc( 0.3em + 1.2vw)";
+//     document.getElementById("MozSahyadri").style.fontSize = "calc(0.80em + 5.0vw)";
+//     document.getElementById("NavList").style.margin = "7vw 0vw";
+//     document.getElementById("MozSahyadri").style.margin = "1.5vw 0vw";  
 //   }
-// }
+// } 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import "../styles/index.css"
 
 const IndexPage = () => (
   <Layout>
-    <p style={{margin:0,}}>Official Website of the Mozilla Campus Club at Sahyadri College of Engineering and Management</p>
+    <p>Official Website of the Mozilla Campus Club at Sahyadri College of Engineering and Management</p>
     <div class="col-md-9">
         <h2 id="heads">What is a Mozilla Campus Club?</h2>
         
@@ -48,5 +48,27 @@ const IndexPage = () => (
     <Link to="/events">Join us</Link>
   </Layout>
 )
+
+//Uncomment during development
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  //minimized
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("Nav").style.padding = "0.2vw";
+    document.getElementById("NavList").style.fontSize = "calc(0.10em + 1.0vw)";
+    document.getElementById("MozSahyadri").style.fontSize = "calc(0.25em + 3.0vw)";
+    document.getElementById("NavList").style.margin = "4.8vw 0vw";
+    document.getElementById("MozSahyadri").style.margin = "calc(0.4em + 0.8vw) 0vw";  
+  }
+  //maximized 
+  else {
+    document.getElementById("Nav").style.padding = "calc(5.0rem + 10.0vw)";
+    document.getElementById("NavList").style.fontSize = "calc( 0.3em + 1.2vw)";
+    document.getElementById("MozSahyadri").style.fontSize = "calc(0.80em + 5.0vw)";
+    document.getElementById("NavList").style.margin = "7vw 0vw";
+    document.getElementById("MozSahyadri").style.margin = "1.5vw 0vw";  
+  }
+} 
 
 export default IndexPage
